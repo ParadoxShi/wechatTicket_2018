3a84544ce7bc3ec44007ce1b253816443e219f68
ParadoxShi <shis2016th@163.com>
2018-10-17 00:32:46 +0800

    fixed bugs in api 4-10
    tested on PC, I'm sure they are safe

--- adminpage/views.py
+++ adminpage/views.py
@@ -4,6 +4,7 @@ from codex.baseview import APIView
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from wechat.models import Activity
+from wechat.models import Ticket
 import datetime
 from django.conf import settings
 import os
@@ -63,16 +64,18 @@ class ActivityList(APIView):
         else:
             raise ValidateError('Sorry, you are not logged in.')
 
+
 class ActivityDelete(APIView):
 
     def post(self):
         self.check_input('id')
         try:
-            activity_to_be_deleted = Activity.objects.filter(id=self.input['id'])
+            activity_to_be_deleted = Activity.objects.get(id=self.input['id'])
         except Activity.DoesNotExist:
             raise ValidateError('The activity to be deleted does not exist.')
         activity_to_be_deleted.status = Activity.STATUS_DELETED
         activity_to_be_deleted.save()
+
 
 class ActivityCreate(APIView):
     def post(self):
@@ -90,6 +93,7 @@ class ActivityCreate(APIView):
                                            book_start=self.input['bookStart'],
                                            book_end=self.input['bookEnd'],
                                            total_tickets=self.input['totalTickets'],
+                                           remain_tickets=self.input['totalTickets'],
                                            status=self.input['status']
                                           )
             return item.id
@@ -104,9 +108,10 @@ class ImageUpload(APIView):
         self.check_input('image')
         try:
             img = self.input['image'][0]
+            print(img)
             path = os.path.join(settings.IMAGE_PATH, img.name)
             with open(path, 'wb') as img_path:
-                for p in img.chunks:
+                for p in img.chunks():
                     img_path.write(p)
             url = settings.SITE_DOMAIN + '/img/Upload/' + img.name
             return url
@@ -121,10 +126,27 @@ class ActivityDetail(APIView):
             raise ValidateError('You need to login first.')
         self.check_input('id')
         try:
-            item = Activity.get_by_id(self.input['id'])
-            item['currentTime'] = time.time()
-            item['bookedTickets'] = item.total_tickets - item.remain_tickets
-            item['usedTickets'] = Ticket.objects.filter(activity=item.name, status=Ticket.STATUS_USED)
+            activity = Activity.get_by_id(self.input['id'])
+            item = {}
+
+            item['name'] = activity.name
+            item['key'] = activity.key
+            item['description'] = activity.description
+            item['startTime'] = activity.start_time.timestamp()
+            item['endTime'] = activity.end_time.timestamp()
+            item['place'] = activity.place
+            item['bookStart'] = activity.book_start.timestamp()
+            item['bookEnd'] = activity.book_end.timestamp()
+            item['totalTickets'] = activity.total_tickets
+            if activity.status == Activity.STATUS_PUBLISHED:
+                item['status'] = 1
+            else:
+                item['status'] = 0
+            item['picUrl'] = activity.pic_url
+
+            item['currentTime'] = datetime.datetime.now().timestamp()
+            item['bookedTickets'] = activity.total_tickets - activity.remain_tickets
+            item['usedTickets'] = Ticket.objects.filter(activity_id=activity.id, status=Ticket.STATUS_USED).count()
             return item
         except Exception as e:
             raise MySQLError('Query activity detail failed!')
@@ -133,7 +155,7 @@ class ActivityDetail(APIView):
         if not self.request.user.is_authenticated():
             raise ValidateError('You need to login first.')
         #  self.check_input('id', 'name', 'place', 'description', 'picUrl',
-        #                   'startTime', 'endTime', 'bookStart', 'bookend', 'totalTickets', 'status')
+        #                   'startTime', 'endTime', 'bookStart', 'bookEnd', 'totalTickets', 'status')
         try:
             activity = Activity.get_by_id(self.input['id'])
             if activity.status == Activity.STATUS_SAVED:
@@ -141,16 +163,23 @@ class ActivityDetail(APIView):
                 activity.place = self.input['place']
             activity.description = self.input['description']
             activity.pic_url = self.input['picUrl']
-            current_time = time.time()
-            if current_time < activity.end_time:
+            current_time = datetime.datetime.now().timestamp()
+
+            # 这一段的顺序比较重要，牵涉到数据库date对象和str对象的转换
+
+            if current_time < activity.start_time.timestamp():
+                activity.book_end = self.input['bookEnd']
+
+            if current_time < activity.end_time.timestamp():
                 activity.start_time = self.input['startTime']
                 activity.end_time = self.input['endTime']
+
+            if current_time < activity.book_start.timestamp():
+                activity.total_tickets = self.input['totalTickets']
+
             if activity.status is not Activity.STATUS_SAVED:
                 activity.book_start = self.input['bookStart']
-            if current_time < activity.start_time:
-                activity.book_end = self.input['bookend']
-            if current_time < activity.book_start:
-                activity.total_tickets = self.input['totalTickets']
+
             if activity.status is not Activity.STATUS_PUBLISHED:
                 activity.status = self.input['status']
             activity.save()
