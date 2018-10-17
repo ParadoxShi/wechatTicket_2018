a636412b72db1c6fc404a70e010ca0c6f0055a39
yf_wang <yf_wang16@163.com>
2018-10-17 11:05:03 +0800

    fixed a foolish bug
    add urls

--- adminpage/urls.py
+++ adminpage/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     url(r'^activity/create$', ActivityCreate.as_view()),
     url(r'^image/upload/?$', ImageUpload.as_view()),
     url(r'^activity/detail/?$', ActivityDetail.as_view()),
+    url(r'^activity/menu/?$', WechatTicketMenu.as_view()),
+    url(r'^activity/checkin/?$', CheckTicket.as_view())
 ]
