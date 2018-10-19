4990bcfdf1fb4abd71d608afb236d880d63a440e
ParadoxShi <shis2016th@163.com>
2018-10-19 18:56:06 +0800

    test for api 1-3

--- userpage/tests.py
+++ userpage/tests.py
@@ -15,110 +15,162 @@ sys_superuser = {
     "password": 'password'
 }
 
-act_published = {"id": 1,
-                 "name": 'published',
-                 "key": 'key1',
-                 "place": 'hall',
-                 "description": 'description',
-                 "startTime": timezone.now() + timedelta(1000),
-                 "endTime": timezone.now() + timedelta(2000),
-                 "bookStart": timezone.now() - timedelta(500),
-                 "bookEnd": timezone.now() + timedelta(500),
-                 "totalTickets": 1000,
-                 "status": Activity.STATUS_PUBLISHED,
-                 "remainTickets": 1000,
-                 "picUrl": "test_url"
+act_published = {
+    "id": 1,
+    "name": 'published',
+    "key": 'key1',
+    "place": 'hall',
+    "description": 'description',
+    "startTime": timezone.now() + timedelta(1000),
+    "endTime": timezone.now() + timedelta(2000),
+    "bookStart": timezone.now() + timedelta(500),
+    "bookEnd": timezone.now() + timedelta(800),
+    "totalTickets": 1000,
+    "status": Activity.STATUS_PUBLISHED,
+    "remainTickets": 1000,
+    "picUrl": "test_url"
 }
 
-ticket_valid = {"student_id": '2016010101',
-                "unique_id": '201601010101',
-                "activity": act_published,
-                "status": Ticket.STATUS_VALID
+act_saved = {
+    "id": 2,
+    "name": 'saved',
+    "key": 'key2',
+    "place": 'hall',
+    "description": 'description',
+    "startTime": timezone.now() + timedelta(1000),
+    "endTime": timezone.now() + timedelta(2000),
+    "bookStart": timezone.now() + timedelta(500),
+    "bookEnd": timezone.now() + timedelta(800),
+    "totalTickets": 1000,
+    "status": Activity.STATUS_SAVED,
+    "remainTickets": 1000,
+    "picUrl": "test_url"
 }
 
-class userBindTest(TestCase):
+test_user = {
+    'open_id': '0001',
+    "student_id": '2016010101',
+    "password": '201601010101'
+}
+
+never_existed_user = {
+    'openid': '0002',
+    "student_id": '2016012345',
+    "password": '201601010101'
+}
+
+
+ticket_valid = {
+    "open_id": test_user['open_id'],
+    "student_id": test_user['student_id'],
+    "unique_id": '201601010101',
+    "status": Ticket.STATUS_VALID
+}
+
+
+def activity_anti_liquid(act):
+    liquid ={
+        "id": act['id'],
+        "name": act['name'],
+        "key": act['key'],
+        "place": act['place'],
+        "description": act['description'],
+        "start_time": act['startTime'],
+        "end_time": act['endTime'],
+        "book_start": act['bookStart'],
+        "book_end": act['bookEnd'],
+        "total_tickets": act['totalTickets'],
+        "status": act['status'],
+        "remain_tickets": act['remainTickets'],
+        "pic_url": act["picUrl"]
+    }
+    return liquid
+
+
+class UserBindTest(TestCase):
     """
     Test For API 1
     """
-    student_id = '2016010101'
-    password = 'password'
     def setUp(self):
-        djangoUser.objects.create_superuser(sys_superuser['username'], sys_superuser['email'],sys_superuser['password'])
+        User.objects.get_or_create(open_id=test_user['open_id'])
         self.cl = Client()
-        self.cl.post('api/u/login',sys_superuser)
 
     def tearDown(self):
-        self.cl.post('/api/u/logout',sys_superuser)
+        User.objects.get(open_id=test_user['open_id']).delete()
 
     def test_getBindState(self):
-        res = self.cl.get('api/u/user/bind')
+        res = self.cl.get('/api/u/user/bind', {'openid': test_user['open_id']})
         res_content = res.content.decode('utf-8')
         self.assertEqual(json.loads(res_content)['data'], '')
-        # try to get bind state before binding
 
-    def test_bind(self):
-        res = self.cl.post('api/u/user/bind',student_id,password)
+        res = self.cl.post('/api/u/user/bind', {'openid': test_user['open_id'], 'student_id': test_user['student_id'],
+                                                'password': test_user['password']})
         res_content = res.content.decode('utf-8')
         self.assertEqual(json.loads(res_content)['code'], 0)
-        # try to bind
 
-    def test_getBindState_2(self):
-        res = self.cl.get('api/u/user/bind')
+        res = self.cl.get('/api/u/user/bind', {'openid': test_user['open_id']})
         res_content = res.content.decode('utf-8')
-        self.assertEqual(json.loads(res_content)['data'], '2016010101')
+        self.assertEqual(json.loads(res_content)['data'], test_user['student_id'])
         # try to get bind state after binding
 
-class activityDetailTest(TestCase):
+
+class ActivityDetailTest(TestCase):
     """
     Test For API 2
     """
     def setUp(self):
-        djangoUser.objects.create_superuser(sys_superuser['username'], sys_superuser['email'],sys_superuser['password'])
-        Activity(act_published).save()
+        act_p = activity_anti_liquid(act_published)
+        act_s = activity_anti_liquid(act_saved)
+        Activity(**act_p).save()
+        Activity(**act_s).save()
         self.cl = Client()
-        self.cl.post('api/u/login',sys_superuser)
 
     def tearDown(self):
         Activity.objects.all().delete()
-        self.cl.post('/api/u/logout',sys_superuser)
 
     def test_getActivityDetail(self):
-        res = self.cl.get('api/u/activity/detail',1)
+        res = self.cl.get('/api/u/activity/detail', {'id': 1})
         res_content = res.content.decode('utf-8')
         self.assertEqual(json.loads(res_content)['code'], 0)
         # try to get detail of a exist activity
 
     def test_getActivityDetail_2(self):
-        res = self.cl.get('api/u/activity/detail',-1)
+        res = self.cl.get('/api/u/activity/detail', {'id': 2})
         res_content = res.content.decode('utf-8')
         self.assertNotEqual(json.loads(res_content)['code'], 0)
         # try to get detail of a non-existent activity
 
-class ticketDetailTest(TestCase):
+
+class TicketDetailTest(TestCase):
     """
     Test For API 3
     """
     def setUp(self):
-        djangoUser.objects.create_superuser(sys_superuser['username'], sys_superuser['email'],sys_superuser['password'])
-        Activity(act_published).save()
-        Ticket(ticket_valid).save()
+        act_p = activity_anti_liquid(act_published)
+        Activity(**act_p).save()
+        Ticket.objects.create(
+            student_id=ticket_valid['student_id'],
+            unique_id=ticket_valid['unique_id'],
+            activity=Activity.objects.get(id=act_p['id']),
+            status=Ticket.STATUS_VALID
+        )
+        User(student_id=test_user['student_id'], open_id=test_user['open_id']).save()
         self.cl = Client()
-        self.cl.post('api/u/login',sys_superuser)
 
     def tearDown(self):
         Ticket.objects.all().delete()
         Activity.objects.all().delete()
-        self.cl.post('/api/u/logout',sys_superuser)
+        User.objects.all().delete()
 
-    def test_getTicketDetail(self):
-        res = self.cl.get('api/u/ticket/detail','201601010101')
+    def test_getValidTicketDetail(self):
+        res = self.cl.get('/api/u/ticket/detail', {'openid': ticket_valid['open_id'], 'ticket': ticket_valid['unique_id']})
         res_content = res.content.decode('utf-8')
         self.assertEqual(json.loads(res_content)['code'], 0)
         # try to get detail of a exist ticket
 
-    def test_getTicketDetail_2(self):
-        res = self.cl.get('api/u/ticket/detail','-1')
+    def test_getInvalidTicketDetail(self):
+        res = self.cl.get('/api/u/ticket/detail', {'openid': ticket_valid['open_id'], 'ticket': 'asdfoa2349'})
         res_content = res.content.decode('utf-8')
         self.assertNotEqual(json.loads(res_content)['code'], 0)
         # try to get detail of a non-existent ticket
-        
+
--- userpage/views.py
+++ userpage/views.py
@@ -21,14 +21,11 @@ class UserBind(APIView):
         if len(User.objects.filter(student_id=self.input['student_id'])) > 0:
             raise ValidateError('You are already in')
 
-
     def get(self):
-        print('here')
         self.check_input('openid')
         return User.get_by_openid(self.input['openid']).student_id
 
     def post(self):
-        print('here')
         self.check_input('openid', 'student_id', 'password')
         user = User.get_by_openid(self.input['openid'])
         self.validate_user()
@@ -48,15 +45,15 @@ class ActivityView(APIView):
             res_item['name'] = item.name
             res_item['key'] = item.key
             res_item['description'] = item.description
-            res_item['startTime'] = item.start_time.timestamp()
-            res_item['endTime'] = item.end_time.timestamp()
+            res_item['startTime'] = int(item.start_time.timestamp())
+            res_item['endTime'] = int(item.end_time.timestamp())
             res_item['place'] = item.place
-            res_item['bookStart'] = item.book_start.timestamp()
-            res_item['bookEnd'] = item.book_end.timestamp()
+            res_item['bookStart'] = int(item.book_start.timestamp())
+            res_item['bookEnd'] = int(item.book_end.timestamp())
             res_item['totalTickets'] = item.total_tickets
             res_item['picUrl'] = item.pic_url
             res_item['remainTickets'] = item.remain_tickets
-            res_item['currentTime'] = datetime.datetime.now().timestamp()
+            res_item['currentTime'] = int(datetime.datetime.now().timestamp())
             return res_item
 
 
@@ -66,5 +63,5 @@ class TicketView(APIView):
         user = User.get_by_openid(self.input['openid'])
         student_id = user.student_id
         detail = Ticket.get_a_ticket(student_id, self.input['ticket'])
-        detail['currentTime'] = datetime.datetime.now().timestamp()
+        detail['currentTime'] = int(datetime.datetime.now().timestamp())
         return detail
